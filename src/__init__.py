650e2065b3ca5eb8a153d0e615ab19c83a0e6e13
Henry Turner <harry.turner@rovco.com>
2020-12-18 11:10:39 +0000

    Added sample routes.

--- src/__init__.py
+++ src/__init__.py
@@ -6,35 +6,31 @@ from flask import Flask, jsonify
 from flask_restx import Resource, Api
 from flask_sqlalchemy import SQLAlchemy
 
-# Instantiate the app.
-app = Flask(__name__)
-
-api = Api(app)
-
-# Set config.
-app_settings = os.getenv('APP_SETTINGS')
-app.config.from_object(app_settings)
-
 # Setup the database.
-db = SQLAlchemy(app)
+db = SQLAlchemy()
 
-# User Model
-class User(db.Model):
-    __tablename__ = 'users'
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    username = db.Column(db.String(128), nullable=False)
-    email = db.Column(db.String(128), nullable=False)
-    active = db.Column(db.Boolean(), default=True, nullable=False)
+def create_app(script_info=None):
 
-    def __init__(self, username, email):
-        self.username = username
-        self.email = email
+    # Instantiate the app.
+    app = Flask(__name__)
 
-class Ping(Resource):
-    def get(self):
-        return {
-            'status': 'success',
-            'message': 'pong!'
-        }
+    # Set config.
+    app_settings = os.getenv('APP_SETTINGS')
+    app.config.from_object(app_settings)
 
-api.add_resource(Ping, '/ping')
+    # Set up extensions.
+    db.init_app(app)
+
+    # register blueprints
+    from src.api.ping import ping_blueprint
+    app.register_blueprint(ping_blueprint)
+
+    from src.api.users import users_blueprint
+    app.register_blueprint(users_blueprint)
+
+    # Shell context for flask cli.
+    @app.shell_context_processor
+    def ctx():
+        return {'app': app, 'db': db}
+
+    return app
